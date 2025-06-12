09da2534700ff51e58aee9879c18f538b4e153ae
wealipay <1293600794@qq.com>
2025-06-12 22:27:17 +0800

    yell

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { createClient } from "@/utils/supabase/server.ts";
+import { createClient } from "./utils/supabase/server.ts";
 export default async function Home() {
   const supabase = createClient();
   const { data: product } = await supabase
     .from("product")
     .select("*")
     .limit(10);
-    console.log(product)
+  console.log(product);
   return <div>home</div>;
 }

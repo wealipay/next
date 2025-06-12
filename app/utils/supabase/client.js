9c6154aef05fa3a939ebf9a37e9b043d16bfbc23
wealipay <1293600794@qq.com>
2025-06-12 22:29:46 +0800

    yell

--- app/utils/supabase/client.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createClient = void 0;
-var ssr_1 = require("@supabase/ssr");
-function createClient() {
-    return (0, ssr_1.createBrowserClient)(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-}
-exports.createClient = createClient;

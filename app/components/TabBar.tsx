16fd36800230cb6cec7d3c5e7078d19892d29029
wealipay <1293600794@qq.com>
2025-06-14 20:01:35 +0800

    hhh

--- app/components/TabBar.tsx
+++ app/components/TabBar.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { createClient } from "@/app/util/supabase/client";
+import { createClient } from "@/util/supabase/client";
 
 export default async function TabBar() {
   const supabase = createClient();
   const { data: product } = await supabase.from("product").select("*");
   return (
     <div>
-      <ul>{{product}}</ul>
+      <ul>{{ product }}</ul>
     </div>
   );
 }

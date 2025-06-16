6d7ee719e901c11c3254ebb60cfc49aa7ce5b437
wealipay <1293600794@qq.com>
2025-06-16 16:12:03 +0800

    hhh

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,10 @@
-// app/layout.tsx
 import { Ma_Shan_Zheng } from "next/font/google";
-
 // 初始化字体
-const zcoolFont = Ma_Shan_Zheng({
+const  font= Ma_Shan_Zheng({
   weight: "400", // 此字体只有400一个字重
   subsets: ["latin"], // 必须包含'latin'
   display: "optional", // 优化加载体验
-  variable: "--font-zcool" // 定义CSS变量
+  variable: "--font-mashan" // 定义CSS变量
 });
 
 export default function RootLayout({
@@ -15,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="zh-CN" className={zcoolFont.className}>
+    <html lang="zh-CN" className={font.className}>
       <body>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-      home龙首页码头
+      home龙首页码头作用**：从 Next.js 中导入 `Metadata` 类型（用于定义页面的 SEO 元数据，如标题、描述等）。
+- **注意**：`import type` 表示仅导入类型（TypeScript 特性），不会增加运行时体积。
     </div>
   );
 }

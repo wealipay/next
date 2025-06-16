6b7d7cd7bd5546c5d3dffb22bf36347c4b824335
wealipay <1293600794@qq.com>
2025-06-16 15:44:18 +0800

    hhh

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,19 @@
 // app/layout.tsx
-import { ZCOOL_QingKe_HuangYou } from 'next/font/google';
+import { ZCOOL_QingKe_HuangYou } from "next/font/google";
 
 // 初始化字体
 const zcoolFont = ZCOOL_QingKe_HuangYou({
-  weight: '400', // 此字体只有400一个字重
-  subsets: ['latin'], // 必须包含'latin'
-  display: 'swap', // 优化加载体验
-  variable: '--font-zcool' // 定义CSS变量
+  weight: "400", // 此字体只有400一个字重
+  subsets: ["latin"], // 必须包含'latin'
+  display: "swap", // 优化加载体验
+  variable: "--font-zcool" // 定义CSS变量
 });
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="zh-CN" className={zcoolFont.className}>
       <body>{children}</body>

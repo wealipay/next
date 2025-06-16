2b29f6b5d8eaec13041ac81dbc651edff17e0782
wealipay <1293600794@qq.com>
2025-06-16 18:28:21 +0800

    hhh

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,19 @@
 import type { Metadata } from "next";
-import { Ma_Shan_Zheng } from "next/font/google";
+import { Ma_Shan_Zheng, Noto_Sans_SC } from "next/font/google";
 
-const font = Ma_Shan_Zheng({
+const font1 = Ma_Shan_Zheng({
   weight: "400",
   subsets: ["latin"],
   display: "optional",
   variable: "--font-mashan",
   preload: true
+});
+
+const font2 = Noto_Sans_SC({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  display: "swap",
+  variable: "--font-noto"
 });
 
 export const metadata: Metadata = {
@@ -27,7 +34,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="zh-CN" className={font.className}>
+    <html lang="zh-CN" className={`${font1.className} ${font2.className}`}>
       <body className="h-dvh h-full">{children}</body>
     </html>
   );

d200a64605d06506c5c61cfe723f8294aac66213
wealipay <1293600794@qq.com>
2025-06-17 20:08:29 +0800

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import NavBar from "@/components/NavBar";
+import NavBar from '../components/NavBar';
 export default function Home() {
-  return <NavBar></NavBar>;
+  return <NavBar title="hhh"></NavBar>;
 }

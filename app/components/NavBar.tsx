bc3e2317f2eaa8367c99e9df9e526ae58c5179ef
wealipay <1293600794@qq.com>
2025-06-18 10:33:44 +0800

    yell

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -11,7 +11,7 @@ export default function NavBar({
   blue?: boolean;
 }) {
   return (
-    <div>
+    <>
       <div className={`fixed inset-x-0 top-0 ${blue ? "bg-red-900" : ""}`}>
         <div className="h-11 flex justify-between items-center px-3">
           <div>{left}</div>
@@ -20,8 +20,9 @@ export default function NavBar({
           </span>
           <div>{right}</div>
         </div>
-        <div className="h-11"></div>
+        
       </div>
-    </div>
+      <div className="h-11"></div>
+    </>
   );
 }

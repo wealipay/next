dabbf1e31bf89623364859dfcb3a6504ad82aff9
wealipay <1293600794@qq.com>
2025-06-18 14:06:49 +0800

    yee

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -12,7 +12,7 @@ export default function NavBar({
 }) {
   return (
     <>
-      <div className={`fixed inset-x-0 top-0 ${blue ? "bg-red-900" : ""}`}>
+      <div className={`fixed inset-x-0 top-0 ${blue ? "bg-navbar" : ""}`}>
         <div className="h-11 flex justify-between items-center px-3">
           <div>{left}</div>
           <span className="font-bold w-fit h-fit absolute inset-0 m-auto text-white">

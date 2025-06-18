2c3da0c789603d949135d39645910e9e22059c79
wealipay <1293600794@qq.com>
2025-06-18 10:02:23 +0800

    yell

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -5,14 +5,12 @@ export default function NavBar({
   blue
 }: {
   children: React.ReactNode;
-  title: string;
+  title: React.ReactNode;
   blue: boolean;
 }) {
   return (
     <>
-      <div
-        className={`fixed inset-x-0 top-0 ${blue ? 'bg-red-900' : ''}`}
-      >
+      <div className={`fixed inset-x-0 top-0 ${blue ? "bg-red-900" : ""}`}>
         <div className="h-11 flex justify-between items-center px-3">
           <span class="font-bold w-fit h-fit absolute inset-0 m-auto text-white">
             {{ title }}

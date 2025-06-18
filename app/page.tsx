6e81d8133a830c790d4205230f9b85549527067d
wealipay <1293600794@qq.com>
2025-06-18 10:43:43 +0800

    yell

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import NavBar from "./components/NavBar";
 export default function Home() {
   return (
     <div>
-      <NavBar blue left={<div>yell</div>}>
+      <NavBar blue left={<div>磊哥</div>} right={<div>王良</div>}>
         支付宝薅羊毛
       </NavBar>
     </div>

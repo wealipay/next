a7b8d0eab144a9aa148c098ce7d4babf153670ce
wealipay <1293600794@qq.com>
2025-06-19 13:49:03 +0800

    bhb

--- app/components/LsItem.tsx
+++ app/components/LsItem.tsx
@@ -7,7 +7,7 @@ interface LsItemProps {
   description: string;
   lg?: boolean;
 }
-export default function LsItem({ left,right,title,description,lg }: LsItemProps) {
+export default function LsItem({ left,children,title,description,lg }: LsItemProps) {
   <div
     className={`ls-item bg-white active:bg-black/10 pl-3 flex items-center ${
       lg ? "h-16" : "h-12"

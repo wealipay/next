a6e73bd29cbd26351472f4261c6006b26caed41a
wealipay <1293600794@qq.com>
2025-06-19 15:25:14 +0800

    bhb

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,35 @@
+import LsItem from "./LsItem";
+import React from "react";
+import Image from "next/image";
+
+interface AvatarProps {
+  unread?: React.ReactNode;
+  name: React.ReactNode;
+  msg: React.ReactNode;
+}
+import src from "../assets/images/launch.jpg";
+export default function Avatar({
+  unread,
+  name,
+  msg
+}: AvatarProps) {
+  return (
+    <LsItem title={name} description={msg} lg={true}>
+      <div className="h-12 w-12 relative">
+        <div
+          v-if="unread > 0"
+          className="rounded-lg h-4 text-white min-w-4 text-xs px-1 bg-red absolute -right-1 -top-1 flex items-center"
+        >
+          {unread}
+        </div>
+        <Image
+          width={100}
+          height={100}
+          src={src}
+          alt=""
+          className="w-full h-full object-cover rounded"
+        />
+      </div>
+    </LsItem>
+  );
+}
--- app/components/LsItemAvatar.tsx
+++ app/components/LsItemAvatar.tsx
@@ -1,3 +1,4 @@
+import Avatar from './Avatar';
 import LsItem from "./LsItem";
 import React from "react";
 import Image from "next/image";
@@ -17,21 +18,8 @@ export default function LsItemAvatar({
 }: LsItemAvatarProps) {
   return (
     <LsItem title={name} description={msg} lg={true}>
-      <div className="h-12 w-12 relative">
-        <div
-          v-if="unread > 0"
-          className="rounded-lg h-4 text-white min-w-4 text-xs px-1 bg-red absolute -right-1 -top-1 flex items-center"
-        >
-          {unread}
-        </div>
-        <Image
-          width={100}
-          height={100}
-          src={src}
-          alt=""
-          className="w-full h-full object-cover rounded"
-        />
-      </div>
+<Avatar />
+
       {children}
     </LsItem>
   );

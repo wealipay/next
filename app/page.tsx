40a839fa2a00f798d8bcf93c7846070918c4f64c
wealipay <1293600794@qq.com>
2025-06-19 23:11:53 +0800

    bhb

--- app/page.tsx
+++ app/page.tsx
@@ -9,35 +9,83 @@ export default function Home() {
     <div>
       <NavBar left="球球" right="哈哈哈就" blue>
         支付宝薅羊毛
-      </NavBar><Ls>
-      <LsItemAvatar
-        left={
-          <div className="h-12 w-12  relative">
-            <div
-              v-if="unread > 0"
-              className="rounded-lg h-4 text-white min-w-4 text-xs px-1 bg-red absolute -right-1 -top-1 flex items-center"
-            >
-              5
+      </NavBar>
+      <Ls>
+        <LsItemAvatar
+          left={
+            <div className="h-12 w-12  relative">
+              <div
+                v-if="unread > 0"
+                className="rounded-lg h-4 text-white min-w-4 text-xs px-1 bg-red absolute -right-1 -top-1 flex items-center"
+              >
+                5
+              </div>
+              <Image
+                width={100}
+                height={100}
+                src={src}
+                alt=""
+                className="w-full h-full object-cover rounded"
+              />
             </div>
-            <Image
-              width={100}
-              height={100}
-              src={src}
-              alt=""
-              className="w-full h-full object-cover rounded"
-            />
+          }
+          name="**雄"
+          msg="领取了5.88元红包"
+        >
+          <div className="text-xs order-3 text-gray-6 text-right">
+            5月20 05:08
           </div>
-        }
-        name="**雄"
-        msg="领取了5.88元红包"
-      >
-        <div className="text-xs order-3 text-gray-6 text-right">
-          5月20 05:08
-        </div>
-      </LsItemAvatar>
-      <LsItemAvatar name="**雄" msg="领取了5.88元红包">
-        <div className="text-xs  text-gray-6">5月20 05:08</div>
-      </LsItemAvatar></Ls>
+        </LsItemAvatar>
+                <LsItemAvatar
+          left={
+            <div className="h-12 w-12  relative">
+              <div
+                v-if="unread > 0"
+                className="rounded-lg h-4 text-white min-w-4 text-xs px-1 bg-red absolute -right-1 -top-1 flex items-center"
+              >
+                5
+              </div>
+              <Image
+                width={100}
+                height={100}
+                src={src}
+                alt=""
+                className="w-full h-full object-cover rounded"
+              />
+            </div>
+          }
+          name="**雄"
+          msg="领取了5.88元红包"
+        >
+          <div className="text-xs order-3 text-gray-6 text-right">
+            5月20 05:08
+          </div>
+        </LsItemAvatar>        <LsItemAvatar
+          left={
+            <div className="h-12 w-12  relative">
+              <div
+                v-if="unread > 0"
+                className="rounded-lg h-4 text-white min-w-4 text-xs px-1 bg-red absolute -right-1 -top-1 flex items-center"
+              >
+                5
+              </div>
+              <Image
+                width={100}
+                height={100}
+                src={src}
+                alt=""
+                className="w-full h-full object-cover rounded"
+              />
+            </div>
+          }
+          name="**雄"
+          msg="领取了5.88元红包"
+        >
+          <div className="text-xs order-3 text-gray-6 text-right">
+            5月20 05:08
+          </div>
+        </LsItemAvatar>
+      </Ls>
     </div>
   );
 }

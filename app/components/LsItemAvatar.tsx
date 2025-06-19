099e549a1f432bcd54e2c9b3cdae8915fe66d880
wealipay <1293600794@qq.com>
2025-06-19 22:55:05 +0800

    bhb

--- app/components/LsItemAvatar.tsx
+++ app/components/LsItemAvatar.tsx
@@ -3,14 +3,12 @@ import React from "react";
 interface LsItemAvatarProps {
   left?: React.ReactNode;
   children?: React.ReactNode;
-  unread?: React.ReactNode;
   name: React.ReactNode;
   msg: React.ReactNode;
 }
 export default function LsItemAvatar({
   left,
   children,
-  unread,
   name,
   msg
 }: LsItemAvatarProps) {

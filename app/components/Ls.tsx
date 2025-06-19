5512defa55b984f908950addc1a798939b91b7df
wealipay <1293600794@qq.com>
2025-06-19 15:08:39 +0800

    bhb

--- app/components/Ls.tsx
+++ app/components/Ls.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 
-interface LsProps {
-  children: React.ReactNode;
-}
-export default function Ls({ children }: LsProps) {
-  return <div className="ls border-y border-y-gray-6">{children}
+export default function Ls() {
+  return <div className="ls border-y border-y-gray-6">
   </div>;
 }

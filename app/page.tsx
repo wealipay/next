59e5a3352ce2875d55156c5d4b682f8c8cf07cbc
wealipay <1293600794@qq.com>
2025-06-19 19:07:07 +0800

    bhb

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
-import LsItemAvatar from '../components/LsItemAvatar';
-import NavBar from "./components/NavBar";
-import Ls from "./components/Ls";
+import LsItemAvatar from "../components/LsItemAvatar";
+import NavBar from "../components/NavBar";
+import Ls from "../components/Ls";
 
 export default function Home() {
   return (
     <div>
       <NavBar blue>支付宝薅羊毛</NavBar>
       <Ls>
-        <LsItemAvatar name="**雄" msg="领取了5.88元红包" unread="9"
-className="flex justify-between">
+        <LsItemAvatar
+          name="**雄"
+          msg="领取了5.88元红包"
+          unread="9"
+          className="flex justify-between"
+        >
           <span className="text-xs text-gray-6">5月20 05:08</span>
         </LsItemAvatar>
         <LsItemAvatar name="**雄" msg="领取了5.88元红包" unread="9">

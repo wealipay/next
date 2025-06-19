4a0ab822e7f2acc94297aa36dcc0a6e96e0e72e4
wealipay <1293600794@qq.com>
2025-06-19 13:38:35 +0800

    bhb

--- app/components/LsItemAvatar.tsx
+++ app/components/LsItemAvatar.tsx
@@ -1,5 +1,6 @@
 import LsItem from "./LsItem";
 import React from "react";
+import Image from "next/image";
 
 interface LsItemAvatarProps {
   children?: React.ReactNode;
@@ -17,14 +18,14 @@ export default function LsItemAvatar({
 }: LsItemAvatarProps) {
   return (
     <LsItem title={nickname} description={msg} lg={true}>
-      <div class="h-12 w-12 relative">
+      <div className="h-12 w-12 relative">
         <div
           v-if="unread > 0"
-          class="rounded-lg h-4 text-white min-w-4 text-xs px-1 bg-red absolute -right-1 -top-1 flex items-center"
+          className="rounded-lg h-4 text-white min-w-4 text-xs px-1 bg-red absolute -right-1 -top-1 flex items-center"
         >
           {unread}
         </div>
-        <img src={src} alt="" class="w-full h-full object-cover rounded" />
+        <Image width={100} height={100} src={src} alt="" className="w-full h-full object-cover rounded" />
       </div>
       {children}
     </LsItem>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-import LsItemAvatar from './components/LsItemAvatar';
+import LsItemAvatar from "./components/LsItemAvatar";
 import NavBar from "./components/NavBar";
-import Image from "next/image";
 import launch from "./assets/images/launch.jpg";
 import Ls from "./components/Ls";
 
@@ -9,9 +8,12 @@ export default function Home() {
     <div>
       <NavBar blue>支付宝薅羊毛</NavBar>
       <Ls>
-        <LsItemAvatar nickname="微信" msg="领取了红包" src={src} unread="9">
-          
-        </LsItemAvatar>
+        <LsItemAvatar
+          nickname="微信"
+          msg="领取了红包"
+          src={launch}
+          unread="9"
+        ></LsItemAvatar>
       </Ls>
     </div>
   );

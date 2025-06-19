b6b1790f9225905c0903caa3d94688cbc6906062
wealipay <1293600794@qq.com>
2025-06-19 16:50:49 +0800

    bhb

--- app/components/LsItem.tsx
+++ app/components/LsItem.tsx
@@ -6,14 +6,19 @@ interface LsItemProps {
   description: React.ReactNode;
   lg?: boolean;
 }
-export default function LsItem({ children, title, description, lg }: LsItemProps) {
+export default function LsItem({
+  children,
+  title,
+  description,
+  lg
+}: LsItemProps) {
   return (
     <div
       className={`ls-item bg-white active:bg-black/10 pl-3 flex items-center ${
         lg ? "h-16" : "h-12"
       }`}
     >
-      <div> {left}</div>
+      <div> {children}</div>
       <div className="r border-b border-b-gray-6 self-stretch items-center flex flex-1 ml-3 pr-3">
         <div className="flex flex-1 flex-col justify-center mr-3">
           <span>{title}</span>

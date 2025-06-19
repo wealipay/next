0f1b7720e8bd39de8791a00c1772430711c0a03b
wealipay <1293600794@qq.com>
2025-06-19 22:31:02 +0800

    bhb

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ import Ls from "./components/Ls";
 export default function Home() {
   return (
     <div>
-      <NavBar  left={hg} right={ggg} blue>支付宝薅羊毛</NavBar>
+      <NavBar left=
+"球球" right="哈哈哈就" blue>
+        支付宝薅羊毛
+      </NavBar>
       <Ls>
         <LsItemAvatar name="**雄" msg="领取了5.88元红包" unread="9">
           <div className="text-xs order-3 text-gray-6 text-right">

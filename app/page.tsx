7c67dcad1318b47e266620747d3d909a9dffcd06
wealipay <1293600794@qq.com>
2025-06-19 23:42:10 +0800

    bhb

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       </NavBar>
       <Ls>
         <LsItemAvatar
-          left={
+          unread={
             <div className="h-12 w-12  relative">
               <div
                 v-if="unread > 0"
@@ -21,15 +21,16 @@ export default function Home() {
                 {unread}
               </div>
 
-              <Image
+
+            </div>
+          }
+          left={              <Image
                 width={100}
                 height={100}
                 src={src}
                 alt=""
                 className="w-full h-full object-cover rounded"
-              />
-            </div>
-          }
+              />}
           name="**雄"
           msg="领取了5.88元红包"
         >

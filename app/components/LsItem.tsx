be235547a0777d30bc0101568d3c8dc8c2212065
wealipay <1293600794@qq.com>
2025-06-19 14:45:37 +0800

    bhb

--- app/components/LsItem.tsx
+++ app/components/LsItem.tsx
@@ -1,29 +1,37 @@
 import React from "react";
 
 interface LsItemProps {
-  right?: React.ReactNode;
+  left?: React.ReactNode;
   children?: React.ReactNode;
   title: React.ReactNode;
   description: React.ReactNode;
   lg?: boolean;
 }
-export default function LsItem({ right,children,title,description,lg }: LsItemProps) {
-  return <div
-    className={`ls-item bg-white active:bg-black/10 pl-3 flex items-center ${
-      lg ? "h-16" : "h-12"
-    }`}
-  >
-    {children}
-    <div className="r border-b border-b-gray-6 self-stretch items-center flex flex-1 ml-3 pr-3">
-      <div className="flex flex-1 flex-col justify-center mr-3">
-        <span>{title}</span>
-        <div v-if="description" className="mt-2 flex">
-          <div className="w-0 flex-1 truncate text-gray-6 text-xs">
-            {description}
+export default function LsItem({
+  left,
+  children,
+  title,
+  description,
+  lg
+}: LsItemProps) {
+  return (
+    <div
+      className={`ls-item bg-white active:bg-black/10 pl-3 flex items-center ${
+        lg ? "h-16" : "h-12"
+      }`}
+    >
+      {left}
+      <div className="r border-b border-b-gray-6 self-stretch items-center flex flex-1 ml-3 pr-3">
+        <div className="flex flex-1 flex-col justify-center mr-3">
+          <span>{title}</span>
+          <div v-if="description" className="mt-2 flex">
+            <div className="w-0 flex-1 truncate text-gray-6 text-xs">
+              {description}
+            </div>
           </div>
         </div>
+        {children}
       </div>
-      {right}
     </div>
-  </div>;
+  );
 }

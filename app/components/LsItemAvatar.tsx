b7415be5e3b223ef45bd6feb39cc45b0b96a34a7
wealipay <1293600794@qq.com>
2025-06-19 22:49:25 +0800

    bhb

--- app/components/LsItemAvatar.tsx
+++ app/components/LsItemAvatar.tsx
@@ -4,34 +4,22 @@ import Image from "next/image";
 import src from "../assets/images/launch.jpg";
 interface LsItemAvatarProps {
   left?: React.ReactNode;
+  children?: React.ReactNode;
   unread?: React.ReactNode;
   name: React.ReactNode;
   msg: React.ReactNode;
 }
 export default function LsItemAvatar({
   left,
+  children,
   unread,
   name,
   msg
 }: LsItemAvatarProps) {
   return (
     <LsItem title={name} description={msg} lg={true}>
-      <div className="h-12 w-12  relative">
-        <div
-          v-if="unread > 0"
-          className="rounded-lg h-4 text-white min-w-4 text-xs px-1 bg-red absolute -right-1 -top-1 flex items-center"
-        >
-          {unread}
-        </div>
-        <Image
-          width={100}
-          height={100}
-          src={src}
-          alt=""
-          className="w-full h-full object-cover rounded"
-        />
-      </div>
-      {left}
+{left}
+      {children}
     </LsItem>
   );
 }

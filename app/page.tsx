ad522a577ba540ccba87abcb7e042776fa9228ff
wealipay <1293600794@qq.com>
2025-06-19 14:03:11 +0800

    bhb

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@ import LsItemAvatar from "./components/LsItemAvatar";
 import NavBar from "./components/NavBar";
 import launch from "./assets/images/launch.jpg";
 import Ls from "./components/Ls";
-const src = new URL("@/assets/images/launch.jpg", import.meta.url).href;
+const src = new URL("./assets/images/launch.jpg", import.meta.url).href;
 
 export default function Home() {
   return (
     <div>
       <NavBar blue>支付宝薅羊毛</NavBar>
       <Ls>
-        <LsItemAvatar src={src} unread="9">ggg</LsItemAvatar>
+        <LsItemAvatar src={src} unread="9">
+          ggg
+        </LsItemAvatar>
       </Ls>
     </div>
   );

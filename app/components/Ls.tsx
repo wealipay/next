8f50a8cf8daa6e05cc1653e5869269b408232454
wealipay <1293600794@qq.com>
2025-06-20 16:07:40 +0800

    yyy

--- app/components/Ls.tsx
+++ app/components/Ls.tsx
@@ -1,19 +1,21 @@
 import React from "react";
+import LsItemAvatar from "./components/LsItemAvatar";
+const src = new URL("./assets/images/launch.jpg", import.meta.url).href;
+const unread = 5555566;
 
-interface LsProps {
-  children: React.ReactNode;
-}
-export default function Ls({ children }: LsProps) {
-  return <div className="ls  border-y border-y-gray-6">
-            <LsItemAvatar
-          unread={unread}
-          src={src}
-          name="**雄"
-          msg="领取了5.88元红包"
-        >
-          <div className="text-xs order-3 text-gray-6 text-right">
-            5月20 05:08
-          </div>
-        </LsItemAvatar>
-  </div>;
+export default function Ls() {
+  return (
+    <div className="ls  border-y border-y-gray-6">
+      <LsItemAvatar
+        unread={unread}
+        src={src}
+        name="**雄"
+        msg="领取了5.88元红包"
+      >
+        <div className="text-xs order-3 text-gray-6 text-right">
+          5月20 05:08
+        </div>
+      </LsItemAvatar>
+    </div>
+  );
 }

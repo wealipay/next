1ea1d8e1ac952bf8ea56e3b97634421f444ac533
wealipay <1293600794@qq.com>
2025-06-20 16:06:33 +0800

    yyy

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,14 @@
 import NavBar from "./components/NavBar";
-import { createClient } from "./utils/supabase/server";
 
 import Ls from "./components/Ls";
-export default async function Home() {
-  const supabase = await createClient();
-  const { data: product } = await supabase
-    .from("product")
-    .select("*")
-    .limit(10);
+export default function Home() {
+  
   return (
     <div>
       <NavBar left="球球" right="哈哈哈就" blue>
         支付宝薅羊毛
       </NavBar>
-      <Ls>
-
-      </Ls>
+      <Ls></Ls>
     </div>
   );
 }

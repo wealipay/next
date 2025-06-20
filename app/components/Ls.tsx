266844288084feba8736e9e346545ee1cc2b59d8
wealipay <1293600794@qq.com>
2025-06-20 17:18:27 +0800

    yyy

--- app/components/Ls.tsx
+++ app/components/Ls.tsx
@@ -14,6 +14,7 @@ export default function Ls({ productList }: Props) {
           src={product.src}
           name="**雄"
           msg="领取了5.88元红包"
+          key={product.id}
         >
           <div className="text-xs order-3 text-gray-6 text-right">
             5月20 05:08

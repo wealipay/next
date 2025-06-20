4eb64123fcd0106d3b21b83666e0d5f2292f276e
wealipay <1293600794@qq.com>
2025-06-20 17:56:49 +0800

    yyy

--- app/components/Ls.tsx
+++ app/components/Ls.tsx
@@ -9,18 +9,19 @@ export default function Ls({ productList }: Props) {
   return (
     <div className="ls  border-y border-y-gray-6">
       {productList.map(product => {
-      return(
-        <LsItemAvatar
-          unread={unread}
-          src={product.src}
-          name="**雄"
-          msg="领取了5.88元红包"
-          key={product.id}
-        >
-          <div className="text-xs order-3 text-gray-6 text-right">
-            5月20 05:08
-          </div>
-        </LsItemAvatar>);
+        return (
+          <LsItemAvatar
+            unread={unread}
+            src={product.src}
+            name={product.name}
+            msg="领取了5.88元红包"
+            key={product.id}
+          >
+            <div className="text-xs order-3 text-gray-6 text-right">
+              5月20 05:08
+            </div>
+          </LsItemAvatar>
+        );
       })}
     </div>
   );

b1ae943914d88290e31086f3b91736cf162945ff
wealipay <1293600794@qq.com>
2025-06-21 08:29:08 +0800

    ggg

--- app/components/LsItem.tsx
+++ app/components/LsItem.tsx
@@ -42,7 +42,7 @@ export default function LsItem({
 
       <div className="r flex border-b border-b-gray-6 self-stretch items-center flex flex-1 ml-3 pr-3">
         <div className="flex  flex-1 flex-col justify-center mr-3">
-          <span>{title}</span>
+          <span>{name}</span>
           <div v-if="type" className="mt-2 flex">
             <div className="w-0 flex-1 truncate text-gray-6 text-xs">
               {acount}

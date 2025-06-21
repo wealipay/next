065e312a77890a24cdc26e21e16c6106f8a5f9ac
wealipay <1293600794@qq.com>
2025-06-21 19:40:27 +0800

    hhh

--- app/components/LsItem.tsx
+++ app/components/LsItem.tsx
@@ -31,7 +31,7 @@ export default function LsItem({
         >
           {unread}
         </div>
-        <div className="h-4 text-red font-bold font-sans min-w-4 text-xs px-1 -translate-x-1/2 -translate-y-1/2  absolute left-1/2 top-1/2 flex items-center">
+        <div className="h-4 text-red font-bold font-sans min-w-4 text-[20px] px-1 -translate-x-1/2 -translate-y-1/2  absolute left-1/2 top-1/2 flex items-center">
           {acount}
         </div>
         <Image

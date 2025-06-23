d661971399f98c12476c2b2638df46baa46b605d
wealipay <1293600794@qq.com>
2025-06-23 19:51:11 +0800

    favicon

---
+++ app/details/[slug]/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+interface DetailsProps {
+  children: React.ReactNode;
+}
+export default function Details({ children }: DetailsProps) {
+  return <>detauls
+    {children}</>;
+}

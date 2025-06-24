8a2f1a4354256ccde364589c2f80b50721851f82
wealipay <1293600794@qq.com>
2025-06-24 22:17:40 +0800

    hhh

--- app/list/layout.tsx
+++ app/list/layout.tsx
@@ -1,14 +1,22 @@
 import NavBar from "../components/NavBar";
 import { RiArrowGoBackLine } from "@remixicon/react";
 import React from "react";
-interface ListLayoutProps {
-  children: React.ReactNode;
-}
-export default function ListLayout({ children }: ListLayoutProps) {
+import { createClient } from "../utils/supabase/server";
+import Ls from "../components/Ls";
+
+export default async function ListLayout({ children }: ListLayoutProps) {
+  const supabase = await createClient();
+  const { data: products } = await supabase
+    .from("product")
+    .select("*")
+    .limit(10);
   return (
     <>
-      <NavBar left={<RiArrowGoBackLine />} blue={true}>ggg</NavBar>
-      {children}
+      <NavBar left={<RiArrowGoBackLine />} blue={true}>
+        ggg
+      </NavBar>
+      {products && <Ls productList={products}></Ls>}
+
     </>
   );
 }

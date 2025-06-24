2c12217fa1019d6b581a2d97a3b5a263d94ee9b0
wealipay <1293600794@qq.com>
2025-06-24 16:17:33 +0800

    RiArrowGoBacjLine

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,6 +1,17 @@
-export default function List() {
+import NavBar from "../components/NavBar";
+import { createClient } from "../utils/supabase/server";
+import Ls from "../components/Ls";
+
+export default async function List() {
+  const supabase = await createClient();
+  const { data: products } = await supabase
+    .from("product")
+    .select("*")
+    .limit(10);
   return (
-    <>
-    </>
+    <div>
+      <NavBar blue>支付宝薅羊毛</NavBar>
+      {products && <Ls productList={products}></Ls>}
+    </div>
   );
 }

9d0dd0cc1bc6056f22eba5dd8c24295451383541
wealipay <1293600794@qq.com>
2025-06-25 12:55:25 +0800

    red

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,4 +1,6 @@
+"use client";
 import React from "react";
+import { useRouter } from "next/navigation";
 export default function NavBar({
   left,
   children,
@@ -10,11 +12,14 @@ export default function NavBar({
   right?: React.ReactNode;
   blue?: boolean;
 }) {
+  const router = useRouter();
   return (
     <>
       <div className={`fixed inset-x-0 top-0 ${blue ? "bg-navbar" : ""}`}>
         <div className="h-11 flex justify-between items-center px-3">
-          <div className="text-white active:text-red">{left}</div>
+          <div onClick={() => {router.back()}} className="text-white active:text-red">
+            {left}
+          </div>
           <span className="font-bold w-fit h-fit absolute inset-0 m-auto text-white">
             {children}
           </span>

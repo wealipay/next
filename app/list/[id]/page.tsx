b62d1b5ad38b5b2554be2077cc17c97fb7293c4c
wealipay <1293600794@qq.com>
2025-06-26 13:33:43 +0800

    ghh

--- app/list/[id]/page.tsx
+++ app/list/[id]/page.tsx
@@ -2,17 +2,22 @@ import { createClient } from "../../utils/supabase/server";
 interface IProduct {
   id: string;
   name: string;
+  // 其他产品字段...
 }
 export default async function ListId() {
   const supabase = await createClient();
-  const { data: products }:IProduct[] = await supabase
+  const { data: products } = await supabase
     .from("product")
     .select("*")
     .limit(10);
+  if (!products || products.length === 0) {
+    return <div>No products found</div>;
+  }
   return (
     <div>
       {products.map(product => (
         <li key={product.id} className="product-item">
+          <h2>{product.id}</h2>
           <h2>{product.name}</h2>
         </li>
       ))}

ab6018b7b5fa93f877d49d34f6bf3cb2f0bc31bb
wealipay <1293600794@qq.com>
2025-06-26 10:15:07 +0800

    hhhh

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -7,5 +7,5 @@ export default async function List() {
     .from("product")
     .select("*")
     .limit(10);
-  return <> gggg</>;
+  return <>{products && <Ls productList={products}></Ls>}</>;
 }

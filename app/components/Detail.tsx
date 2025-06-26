0d38d6b0caf30bff1e057152fea0260b44717b46
wealipay <1293600794@qq.com>
2025-06-26 14:01:36 +0800

    ghh

--- app/components/Detail.tsx
+++ app/components/Detail.tsx
@@ -1,3 +1,12 @@
-export default function Detail() {
-  return (<>ggggh</>)
+import { IProduct } from "../types/products";
+
+type Props = {
+  detail: IProduct[];
+};
+export default function Detail({ detail }: Props) {
+  return <>
+    {detail.map((item)=>{return(
+    <div key={item.id}>{item.id}</div>
+    )})}
+  </>;
 }
--- app/list/[id]/page.tsx
+++ app/list/[id]/page.tsx
@@ -2,10 +2,10 @@ import Detail from "../../components/Detail";
 import { createClient } from "../../utils/supabase/server";
 export default async function ListId() {
   const supabase = await createClient();
-  const { data: products }= await supabase
+  const { data: details } = await supabase
     .from("product")
     .select("*")
     .limit(10);
 
-  return <Detail>{products}</Detail>;
+  return <>{details && <Detail detail={ details}></Detail>}</>;
 }

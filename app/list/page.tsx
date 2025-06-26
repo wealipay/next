326fc43a98935c4b9cd097a2a73913d5c1f0d81f
wealipay <1293600794@qq.com>
2025-06-26 10:25:12 +0800

    hhhh

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,11 +1,15 @@
 import { createClient } from "../utils/supabase/server";
 import Ls from "../components/Ls";
-
-export default async function List() {
+import React from "react";
+interface ListProps {
+  children: React.ReactNode;
+}
+export default async function List({ children }: ListProps) {
   const supabase = await createClient();
   const { data: products } = await supabase
     .from("product")
     .select("*")
     .limit(10);
-  return <>{products && <Ls productList={products}></Ls>}</>;
+  return <>{products && <Ls productList={products}></Ls>}
+  idpage</>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
 import NavBar from "./components/NavBar";
-import { createClient } from "./utils/supabase/server";
-import Ls from "./components/Ls";
 
 export default async function Home() {
-  const supabase = await createClient();
-  const { data: products } = await supabase
-    .from("product")
-    .select("*")
-    .limit(10);
   return (
     <div>
-      <NavBar blue>支付宝薅羊毛</NavBar>
-      {products && <Ls productList={products}></Ls>}
+<NavBar blue>支付宝薅羊毛</NavBar>
     </div>
   );
 }

e7281bc0e24f53dd50bc4e267830ed0fbb727186
wealipay <1293600794@qq.com>
2025-06-26 10:27:05 +0800

    hhhh

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,15 +1,16 @@
 import { createClient } from "../utils/supabase/server";
 import Ls from "../components/Ls";
 import React from "react";
-interface ListProps {
-  children: React.ReactNode;
-}
-export default async function List({ children }: ListProps) {
+
+export default async function List() {
   const supabase = await createClient();
   const { data: products } = await supabase
     .from("product")
     .select("*")
     .limit(10);
-  return <>{products && <Ls productList={products}></Ls>}
-  idpage</>;
+  return (
+    <>
+      {products && <Ls productList={products}></Ls>}
+    </>
+  );
 }

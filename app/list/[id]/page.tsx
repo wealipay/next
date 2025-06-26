f026361bac837e544dbe4354b3becc2ffaa6bb74
wealipay <1293600794@qq.com>
2025-06-26 13:00:57 +0800

    ghh

--- app/list/[id]/page.tsx
+++ app/list/[id]/page.tsx
@@ -1,8 +1,11 @@
 import { createClient } from "../../utils/supabase/server";
-
+import { IProduct } from "../../types/products";
+type Props = {
+  productList: IProduct[];
+};
 export default async function ListId() {
   const supabase = await createClient();
-  const { data: products } = await supabase
+  const { data: products }:Props = await supabase
     .from("product")
     .select("*")
     .limit(10);

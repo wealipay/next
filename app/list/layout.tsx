48e5db18785415d4d77a5dacb6b2ae1158b47831
wealipay <1293600794@qq.com>
2025-06-26 10:12:12 +0800

    hhhh

--- app/list/layout.tsx
+++ app/list/layout.tsx
@@ -1,15 +1,17 @@
 import NavBar from "../components/NavBar";
 import { RiArrowGoBackLine } from "@remixicon/react";
 import React from "react";
+interface ListLayoutProps {
+  children: React.ReactNode;
+}
 
-
-export default async function ListLayout() {
-  
+export default async function ListLayout({children}:ListLayoutProps) {
   return (
     <>
       <NavBar left={<RiArrowGoBackLine />} blue={true}>
         红包领取列表
       </NavBar>
+      {children}
     </>
   );
 }

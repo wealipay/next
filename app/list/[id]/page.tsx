dc36fd46a3b1c29097f6bc0abdbcb306b50801e4
wealipay <1293600794@qq.com>
2025-06-26 15:43:21 +0800

    ghh

--- app/list/[id]/page.tsx
+++ app/list/[id]/page.tsx
@@ -1,8 +1,6 @@
 import Detail from "../../components/Detail";
 import { createClient } from "../../utils/supabase/server";
-interface Product {
-  id: number;
-}
+
 export default async function ListId(id) {
   const supabase = await createClient();
   const { data: details } = await supabase
